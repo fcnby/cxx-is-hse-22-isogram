6ffcfae897b30cdcc4c523f86a58c74c4e910e7f
kiril <kdibragimov@edu.hse.ru>
2022-02-17 23:27:04 +0300

    v1

--- isogram/isogram.cpp
+++ isogram/isogram.cpp
@@ -1,14 +1,29 @@
 #include "isogram.hpp"
-#include <set>
-#include <locale>
+#include <iostream>
 
-// Для фукнций std::isalpha и std::toupper используйте эту локаль
-std::locale locale{""};
+auto is_isogram(std::string const & word) -> bool {
 
-auto is_isogram(std::string const& word) -> bool {
-    // Функция is_isogram получает на вход строку word
-    // Возвращает true, если строка является изограммой,
-    // и false, если не явялется
-    // Код пишите здесь
-    return word.empty();
+    std::string s = word;
+    if (s.empty()) {
+        return true;
+    }
+
+    else {
+
+        for (int i = 0; i <= s.length()-1; i++) {
+            s[i] = tolower(s[i]);
+
+        }
+        std::sort(s.begin(), s.end());
+
+        for (int i = 0; i <= s.length()-1; i++) {
+
+            if ((s[i] == s[i + 1]) and (isalpha(s[i]))) {
+
+                return false;
+
+            }
+        }
+        return true;
+    }
 }
